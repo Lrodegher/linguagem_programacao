c925f1350ade5eb2ec99474eebb471437fb041c0
Lrodegher <145458932+Lrodegher@users.noreply.github.com>
2025-06-04 19:55:30 -0300

    Delete components directory

--- components/cards.py
+++
@@ -1,7 +0,0 @@
-from dash import html
-
-def metric_card(title, value, color="#FF7100"):
-    return html.Div([
-        html.Div(title, className="metric-title", style={"color": color}),
-        html.Div(value, className="metric-value", style={"color": color}),
-    ], className="metric-card")
